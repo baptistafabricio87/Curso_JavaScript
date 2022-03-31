547df749c5929161802cac67c829308d59e03ef7
Fabricio Castro <baptistafabricio87@gmail.com>
2022-03-31 17:39:31 -0300

    Exemplos de implementação do metodo REDUCE.

---
+++ js_modulo2/exemplos_aula05_reduce.js
@@ -0,0 +1,23 @@
+var valores = [1.5, 2, 4, 10];
+
+var calc = function(total, item, indice) {
+    console.log(`T: ${total} Ind: ${indice} Item: ${item}`);
+    return total = total + item;
+}
+
+var somatoria = valores.reduce(calc);
+console.log(somatoria);
+
+// -------------------------
+var valores = [1.5, 2, 4, 10];
+// var calc = function(total, item, indice) {
+//     console.log(`T: ${total} Ind: ${indice} Item: ${item}`);
+//     return total = total + item;
+// }
+var somatoria = valores.reduce(function(total, item, indice) {
+    console.log(`T: ${total} Ind: ${indice} Item: ${item}`);
+    return total + item;
+}, 0);
+console.log(somatoria);
+
+// -------------------------
